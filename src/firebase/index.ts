958a5bd37b7e8faf0187a898b99d2dc48c228531
Harishpatel Harishpatel <harishpatelh450@gmail.com>
2026-05-02 04:17:39 +0000

    Try fixing this error: `Runtime FirebaseError: Firebase: Error (auth/inv

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,14 +1,17 @@
 'use client';
 
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
+import { getFirestore, Firestore } from 'firebase/firestore';
+import { getAuth, Auth } from 'firebase/auth';
 import { firebaseConfig } from './config';
 
-export function initializeFirebase() {
+export function initializeFirebase(): { app: FirebaseApp; db: Firestore; auth: Auth } {
+  // Initialize standard app
   const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+  
   const db = getFirestore(app);
   const auth = getAuth(app);
+
   return { app, db, auth };
 }
 
